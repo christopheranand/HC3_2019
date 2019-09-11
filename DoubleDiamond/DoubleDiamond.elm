ba1ddeb0fd3262cb57718c6f5b9964d821ea4999
Yumna Irfan <yumnairfan99@hotmail.com>
2019-09-10 23:45:06 -0400

    double diamond in progress

---
+++ DoubleDiamond/DoubleDiamond.elm
@@ -0,0 +1,53 @@
+module DoubleDiamond exposing (..)
+
+import GraphicSVG exposing(..)
+import GraphicSVG.EllieApp exposing (..)
+import List
+
+myShapes model = [ 
+                    text "Discover" |> filled red |> notifyTap (Clicked Discover)
+                   
+                 ]
+                ++ 
+                    case model.state of 
+                        PopUp stage -> [rect 40 50 |> filled green 
+                                        , cross |> notifyTap Exit |> move (15,20)
+                                        , txt stage (strStage stage)
+                                      
+                                        ]
+
+                        otherwise -> []
+
+type State = NoPopUp | PopUp Stages  
+
+type Stages = Discover | Define | Develop | Deliver 
+
+type Msg = Tick Float GetKeyState
+          | Clicked Stages 
+          | Exit 
+
+txt stage lst =  group (List.indexedMap (\idx line -> text line 
+                                                        |> size 8 
+                                                        |> centered 
+                                                        |> filled black 
+                                                        |> move (0,10-10*(toFloat idx))) lst)
+
+cross = group [rect 2 7 |> filled red |> rotate (degrees 45)
+               , rect 2 7 |> filled red |> rotate (degrees (-45)) 
+                ]
+
+strStage stage = case stage of 
+                  Discover -> ["Discover", "stuff" ]
+                  Define -> ["Define"]
+                  Develop -> ["Develop"]
+                  Deliver -> ["Deliver"]
+
+update msg model = case msg of
+                     Tick t _ -> { model | time = t }
+                     Clicked stage -> {model | state = PopUp stage }
+                     Exit -> {model | state = NoPopUp }
+
+init = {time = 0, state = NoPopUp }
+main = gameApp Tick { model = init, view = view, update = update, title = "DoubleDiamond" }
+
+view model = collage 192 128 (myShapes model)
