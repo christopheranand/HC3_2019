7b2ed802ea1f9f9a2a0751785f0aa1b5317a76ec
Yumna Irfan <yumnairfan99@hotmail.com>
2019-09-18 19:29:39 -0400

    Finalized double diamond

--- DoubleDiamond/DoubleDiamond.elm
+++ DoubleDiamond/DoubleDiamond.elm
@@ -8,31 +8,32 @@ myShapes model = [
                     diamond |> move (15,0)
                    , diamond |> move (74,0)
                    , textOnDiamond 
-                   , doubleArrow 27 19 "discover" Discover (-45,-30) |> move (-45,-30)
-                   , doubleArrow 27 14 "define" Define (-15,-30) |> move (-15,-30)
-                   , doubleArrow 27 18 "develop" Develop (15,-30) |> move (15,-30)
-                   , doubleArrow 27 15 "deliver" Deliver (45,-30)|> move (45,-30)
-                   , doubleArrow 36 23 "inspiration" Inspiration (-55,-40) |> move (-55,-40)
-                   , doubleArrow 53 18 "ideation" Ideation (-15,-40) |> move (-7,-40)
-                   , doubleArrow 45 33 "implementation" Implementation (15,-40) |> move (45,-40)
+                   , doubleArrow 27 19 "discover" Discover (-45,-30) green |> move (-45,-30)
+                   , doubleArrow 27 14 "define" Define (-15,-30) green |> move (-15,-30)
+                   , doubleArrow 27 18 "develop" Develop (15,-30) green |> move (15,-30)
+                   , doubleArrow 27 15 "deliver" Deliver (45,-30) green |> move (45,-30)
+                   , doubleArrow 36 23 "inspiration" Inspiration (-55,-40) (rgb 0 191 255) |> move (-55,-40)
+                   , doubleArrow 53 18 "ideation" Ideation (-15,-40) (rgb 0 191 255) |> move (-7,-40)
+                   , doubleArrow 45 33 "implementation" Implementation (45,-40) (rgb 0 191 255) |> move (45,-40)
                    ]
                 ++ 
                     case model.state of 
-                        PopUp stage (x,y)-> [card stage (x,y)
+                        PopUp stage (x,y) col-> [card stage (x,y) col
                                               ]
 
                         otherwise -> []
 
-type State = NoPopUp | PopUp Stages (Float,Float)   
+type State = NoPopUp | PopUp Stages (Float,Float) Color  
 
 type Stages = Discover | Define | Develop | Deliver | Inspiration | Ideation | Implementation
 
 type Msg = Tick Float GetKeyState
-          | Clicked Stages (Float,Float)
+          | Clicked Stages (Float,Float) Color
           | Exit 
 
-card stage (x,y)= group [rect 40 45 |> filled green |> makeTransparent 0.9
-                                        , cross |> notifyTap Exit |> move (17,20)
+card stage (x,y) col= group [rect 42 45 |> filled col |> makeTransparent 0.9
+                                        , square 5 |> filled col |> makeTransparent 0.9 |> move (18,20) |> notifyTap Exit
+                                        , cross |> notifyTap Exit |> move (18,20)
                                         , txt (strStage stage)
                                       ] |> move (x,y)
 
@@ -42,7 +43,7 @@ txt lst =  group (List.indexedMap (\idx line -> text line
                                                         |> size 6 
                                                         |> centered 
                                                         |> filled black 
-                                                        |> move (0,12-7*(toFloat idx))) lst)
+                                                        |> move (0,14-7*(toFloat idx))) lst)
 
 cross = group [rect 1 5 |> filled red |> rotate (degrees 45)
                , rect 1 5 |> filled red |> rotate (degrees (-45)) 
@@ -53,13 +54,13 @@ curvedArrow = group [curve (-12,0) [Pull (0,-10) (12,0)]
                    , ngon 3 2 |> filled black |> rotate (degrees (10))
                       |> move (-10,-10)]
 
-doubleArrow len rectLen str stage (x,y)= group [rect len 1 |> filled black
+doubleArrow len rectLen str stage (x,y) col = group [rect len 1 |> filled black
                         , ngon 3 2 |> filled black |> move ((len/2),0)
                         , ngon 3 2 |> filled black |> rotate (degrees (180)) |> move ((-len/2),0)
                         , rect rectLen 2 |> filled white
-                        , text str |> centered |> size 5 |> filled darkBlue 
+                        , text str |> centered |> size 5 |> filled black 
                             |> move (0,-1)
-                            |> notifyTap (Clicked stage (x,y) )
+                            |> notifyTap (Clicked stage (x,y) col )
                         ] 
 arrow x y deg= group [rect 40 1 |> filled black 
                 , ngon 3 2 |> filled black |> move (20,0)
@@ -67,12 +68,12 @@ arrow x y deg= group [rect 40 1 |> filled black
 
 strStage stage = case stage of 
                   Discover -> ["Discover", "the problem", "through", "interviewing", "the end user." ]
-                  Define -> ["Define","the right" , "problem through", "converging to", "an idea"]
-                  Develop -> ["Develop"]
-                  Deliver -> ["Deliver"]
-                  Inspiration -> ["Inspiration"]
-                  Ideation -> ["Ideation"]
-                  Implementation -> ["Implementation"]
+                  Define -> ["Define","the right" , "problem through", "converging to", "an idea."]
+                  Develop -> ["Develop", "prototypes,", "test them", "and iterate", "this process."]
+                  Deliver -> ["Deliver", "The product", "is finalized", "and launched", "to the market."]
+                  Inspiration -> ["Inspiration", "Learn how", "to better", "understand", "people."]
+                  Ideation -> ["Ideation", "Organize and", "analyze research", "to generate ideas", "and refine", "solutions."]
+                  Implementation -> ["Implementation", "Bring your", "solution to", "life and", "maximize its", "impact."]
 
 {-
 discovering the problem through empathizing
@@ -119,7 +120,7 @@ textOnDiamond = group [ text "define" |> size 5 |> filled black
 
 update msg model = case msg of
                      Tick t _ -> { model | time = t }
-                     Clicked stage (x,y) -> {model | state = PopUp stage (x,y) }
+                     Clicked stage (x,y) col -> {model | state = PopUp stage (x,y) col }
                      Exit -> {model | state = NoPopUp }
 
 init = {time = 0, state = NoPopUp }
